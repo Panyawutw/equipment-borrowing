424f378e26c37a416efb6dade7c4872fb7494198
Panyawutw <0854734340za@gmail.com>
2026-09-19 22:50:39 +0700

    style: จัดระเบียบโค้ด

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
-host: process.env.DB_HOST,
-port: Number(process.env.DB_PORT),
-database: process.env.DB_NAME,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-pool.on('error', (err) => {
-console.error('Unexpected PostgreSQL error', err);
+pool.on("error", (err) => {
+  console.error("Unexpected PostgreSQL error", err);
 });
 module.exports = pool;
